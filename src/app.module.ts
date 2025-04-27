be077d5021862219e3e9eafb27c0903b61e3d604
Paulo Rodrigues <pcqrodrigues@gmail.com>
2025-04-27 20:17:05 -0300

    feat(db): added db connection and migration configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DeviceModule } from './device/device.module';
+import { DbModule } from './db/db.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [DeviceModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DeviceModule, DbModule],
   controllers: [AppController],
   providers: [AppService],
 })
